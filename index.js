8b21792182705b3d82936c0e8061d58eb023a85d
Haukur Rosinkranz <hauxir@gmail.com>
2017-10-29 23:19:13 +0100

    server cleanup

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ var WebSocketServer = require("websocket").server;
 var exec = require("child_process").exec;
 var os = require("os");
 var serveStatic = require("serve-static");
-
 var http = require("http");
 
 var files = serveStatic("web", { index: ["index.html"] });
@@ -19,25 +18,29 @@ wsServer = new WebSocketServer({
 
 wsServer.on("request", function(request) {
   var connection = request.accept();
-
   connection.on("message", function(message) {
     if (message.type === "utf8") {
       var json = JSON.parse(message.utf8Data);
-      var echostring = json.action + " " + json.button;
-      if(json.value) {
-        echostring = echostring + " " + json.value
+      if(!(json.action && json.input_name)) return;
+      var input_string = json.action + " " + json.input_name;
+      if (json.value) {
+        input_string = input_string + " " + json.value;
       }
-
-      exec(
+      var process = exec(
         "echo '" +
-          echostring.toUpperCase() +
+          input_string.toUpperCase() +
           "' > '" +
           os.homedir() +
           "/Library/Application Support/Dolphin/Pipes/ctrl" +
           json.controller +
           "'"
       );
+      process.stdout.on("data", function(data) {
+        console.log(data);
+      });
+      process.stderr.on("data", function(data) {
+        console.log(data);
+      });
     }
   });
-  connection.on("close", function(connection) {});
 });
